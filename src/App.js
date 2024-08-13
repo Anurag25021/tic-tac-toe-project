217851f7d0e410d2015f6c1e2bd103913c5e728e
Anurag25021 <160622670+Anurag25021@users.noreply.github.com>
2024-08-13 10:53:55 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './components/tictackto/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe></TicTacToe>
+    </div>
+  );
+}
+
+export default App;
